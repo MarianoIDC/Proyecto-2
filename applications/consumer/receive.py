71de7ae6a84b5886fab742799ad6e9f74a606a2c
MarianoIDC <mmunoz@estudiantec.cr>
2022-05-21 21:04:29 -0600

    Update

--- applications/consumer/receive.py
+++ applications/consumer/receive.py
@@ -2,6 +2,7 @@
 import pika, sys, os
 
 def main():
+<<<<<<< Updated upstream:applications/consumer/receive.py
     PORT = os.getenv('RABBIT_PORT')
     USER = os.getenv('RABBIT_USER')
     PASSWORD = os.getenv('RABBIT_PASSWORD')
@@ -10,14 +11,18 @@ def main():
     credentials = pika.PlainCredentials(USER, PASSWORD)
     parameters = pika.ConnectionParameters(HOST, PORT, '/', credentials)
     connection = pika.BlockingConnection(parameters)
+=======
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+>>>>>>> Stashed changes:receive.py
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='message')
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='message', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
